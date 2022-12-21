f8874ae37c677faf54601f61c6741d4cca215c81
vdaular <vdaular@derkodex.com>
2022-12-21 08:54:40 -0300

    Se agregan pruebas para funciones delegadas y carpetas de módulos

--- tests/Unit/Modaularity.Tests/DelegateCatalogTests.cs
+++ tests/Unit/Modaularity.Tests/DelegateCatalogTests.cs
@@ -167,4 +167,320 @@ public class DelegateCatalogTests
 
 		Assert.Equal("HelloThereNs", moduleType.Namespace);
 	}
+
+	[Fact]
+	public async Task CanConfigureNamespaceUsingGenerator()
+	{
+		var catalog = new DelegateModuleCatalog(new Func<int, Task<bool>>(async i =>
+		{
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            _testOutputHelper.WriteLine("Hello from test");
+
+            return true;
+        }), new DelegateModuleCatalogOptions()
+		{
+			NamespaceNameGenerator = options => "GeneratorNS"
+		});
+
+		await catalog.Initialize();
+
+		var moduleType = catalog.Single().Type;
+
+		Assert.Equal("GeneratorNS", moduleType.Namespace);
+	}
+
+	[Fact]
+	public async Task ByDefaultGeneratedTypeName()
+	{
+		var catalog = new DelegateModuleCatalog(new Func<int, Task<bool>>(async i =>
+		{
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            _testOutputHelper.WriteLine("Hello from test");
+
+            return true;
+        }));
+
+		await catalog.Initialize();
+
+		var moduleType = catalog.Single().Type;
+
+		Assert.Equal("GeneratedType", moduleType.Name);
+	}
+
+	[Fact]
+	public async Task CanConfigureTypeName()
+	{
+		var catalog = new DelegateModuleCatalog(new Func<int, Task<bool>>(async i =>
+		{
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            _testOutputHelper.WriteLine("Hello from test");
+
+            return true;
+        }), new DelegateModuleCatalogOptions()
+		{
+			TypeName = "HelloThereType"
+		});
+
+		await catalog.Initialize();
+
+		var moduleType = catalog.Single().Type;
+
+		Assert.Equal("HelloThereType", moduleType.Name);
+	}
+
+	[Fact]
+	public async Task CanConfigureTypeNameUsingGenerator()
+	{
+		var catalog = new DelegateModuleCatalog(new Func<int, Task<bool>>(async i =>
+		{
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            _testOutputHelper.WriteLine("Hello from test");
+
+            return true;
+        }), new DelegateModuleCatalogOptions()
+		{
+			TypeNameGenerator = options => "GeneratorTypeName"
+		});
+
+		await catalog.Initialize();
+
+		var moduleType = catalog.Single().Type;
+
+		Assert.Equal("GeneratorTypeName", moduleType.Name);
+	}
+
+	[Fact]
+	public async Task CanConfigureGeneratedMethodName()
+	{
+		var options = new DelegateModuleCatalogOptions()
+		{
+			MethodName = "HelloMethod"
+		};
+
+		var catalog = new DelegateModuleCatalog(new Func<int, Task<bool>>(async i =>
+		{
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            _testOutputHelper.WriteLine("Hello from test");
+
+            return true;
+        }), options);
+
+		await catalog.Initialize();
+
+		var moduleType = catalog.Single().Type;
+		var methodInfos = moduleType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+
+		Assert.Equal("HelloMethod", methodInfos.Single().Name);
+	}
+
+	[Fact]
+	public async Task CanConfigureGeneratedMethodNameUsingGenerator()
+	{
+		var options = new DelegateModuleCatalogOptions()
+		{
+			MethodNameGenerator = catalogOptions => "MethodGeneratorName"
+		};
+
+		var catalog = new DelegateModuleCatalog(new Func<int, Task<bool>>(async i =>
+		{
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            _testOutputHelper.WriteLine("Hello from test");
+
+            return true;
+        }), options);
+
+		await catalog.Initialize();
+
+		var moduleType = catalog.Single().Type;
+		var methodInfos = moduleType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+
+		Assert.Equal("MethodGeneratorName", methodInfos.Single().Name);
+	}
+
+	[Fact]
+	public async Task ByDefaultNoConstructorParameters()
+	{
+		var catalog = new DelegateModuleCatalog(new Func<int, Task<bool>>(async i =>
+		{
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            _testOutputHelper.WriteLine("Hello from test");
+
+            return true;
+        }));
+
+		await catalog.Initialize();
+
+		var moduleType = catalog.Single().Type;
+
+		Assert.Single(moduleType.GetConstructors());
+
+		foreach (var constructorInfo in moduleType.GetConstructors())
+			Assert.Empty(constructorInfo.GetParameters());
+	}
+
+	[Fact]
+	public async Task CanConvertParameterToProperty()
+	{
+		var rules = new List<DelegateConversionRule>()
+		{
+			new DelegateConversionRule(info => info.ParameterType == typeof(int), nfo => new ParameterConversion()
+			{
+				ToPublicProperty= true
+			})
+		};
+
+		var catalog = new DelegateModuleCatalog(new Func<int, Task<bool>>(async i =>
+		{
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            _testOutputHelper.WriteLine("Hello from test");
+
+            return true;
+        }), rules);
+
+		await catalog.Initialize();
+
+		var moduleType = catalog.Single().Type;
+
+		Assert.Single(moduleType.GetProperties());
+	}
+
+	[Fact]
+	public async Task CanConvertParameterToConstructorParameter()
+	{
+		var rules = new List<DelegateConversionRule>()
+		{
+			new DelegateConversionRule(info => info.ParameterType == typeof(int), nfo => new ParameterConversion()
+			{
+				ToConstructor = true
+			})
+		};
+
+		var catalog = new DelegateModuleCatalog(new Func<int, Task<bool>>(async i =>
+		{
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            _testOutputHelper.WriteLine("Hello from test");
+
+            return true;
+        }), rules);
+
+		await catalog.Initialize();
+
+		var moduleType = catalog.Single().Type;
+
+		Assert.Single(moduleType.GetConstructors());
+		Assert.Single(moduleType.GetConstructors().Single().GetParameters());
+	}
+
+	[Fact]
+	public async Task CanConvertMultipleParametersToConstructorAndPropertyParameters()
+	{
+		var rules = new List<DelegateConversionRule>()
+		{
+			new DelegateConversionRule(info => info.ParameterType == typeof(int), nfo => new ParameterConversion()
+			{
+				ToConstructor = true
+			}),
+			new DelegateConversionRule(info => info.ParameterType == typeof(string), nfo => new ParameterConversion()
+			{
+				ToPublicProperty = true
+			}),
+			new DelegateConversionRule(info => info.ParameterType == typeof(bool), nfo => new ParameterConversion()
+			{
+				ToPublicProperty = true
+			}),
+			new DelegateConversionRule(info => info.ParameterType == typeof(decimal), nfo => new ParameterConversion()
+			{
+				ToConstructor = true
+			})
+		};
+
+		var catalog = new DelegateModuleCatalog(new Func<int, string, bool, decimal, char, bool>((i, s, arg3, arg4, c) =>
+		{
+			_testOutputHelper.WriteLine("Hello from test");
+
+			return true;
+		}), rules);
+
+		await catalog.Initialize();
+
+		var moduleType = catalog.Single().Type;
+
+		Assert.Single(moduleType.GetConstructors());
+		Assert.Equal(2, moduleType.GetConstructors().Single().GetParameters().Length);
+		Assert.Equal(2, moduleType.GetProperties().Length);
+
+		dynamic obj = Activator.CreateInstance(moduleType, new object[] { 30, new decimal(22) });
+        obj.S = "hello";
+		obj.Arg3 = true;
+
+		var res = obj.Run('g');
+
+		Assert.True(res);
+	}
+
+	[Fact]
+	public async Task CanSetModuleName()
+	{
+		var catalog = new DelegateModuleCatalog(new Action(() =>
+		{
+			_testOutputHelper.WriteLine("Hello from test");
+		}), nameOptions: new()
+		{
+			ModuleNameGenerator = (options, type) => "CustomModule"
+		});
+
+		await catalog.Initialize();
+
+		var module = catalog.Single();
+
+		Assert.Equal("CustomModule", module.Name);
+	}
+
+	[Fact]
+	public async Task CanSetModuleNameAndVersion()
+	{
+		var catalog = new DelegateModuleCatalog(new Action(() =>
+		{
+			_testOutputHelper.WriteLine("Hello from test");
+		}), nameOptions: new()
+		{
+			ModuleNameGenerator = (options, type) => "CustomModule",
+			ModuleVersionGenerator = (options, type) => new Version(2, 3, 5)
+		});
+
+		await catalog.Initialize();
+
+		var module = catalog.Single();
+
+		Assert.Equal("CustomModule", module.Name);
+		Assert.Equal(new Version(2, 3, 5), module.Version);
+	}
+
+	[Fact]
+	public async Task CanConfigureModulesHaveUniqueNames()
+	{
+		var randomGenerator = new Random(Guid.NewGuid().GetHashCode());
+		var nameOptions = new ModuleNameOptions()
+		{
+			ModuleNameGenerator = (options, type) => $"CustomModule{randomGenerator.Next(int.MaxValue)}"
+		};
+
+		var catalog = new DelegateModuleCatalog(new Action(() =>
+		{
+			_testOutputHelper.WriteLine("Hello from test");
+		}), nameOptions: nameOptions);
+
+		var catalog2 = new DelegateModuleCatalog(new Action(() =>
+		{
+			_testOutputHelper.WriteLine("Hello from test");
+		}), nameOptions: nameOptions);
+
+		await catalog.Initialize();
+		await catalog2.Initialize();
+
+		var firstModule = catalog.Single();
+		var secondModule = catalog2.Single();
+
+		Assert.NotEqual(firstModule.Name, secondModule.Name);
+	}
 }
---
+++ tests/Unit/Modaularity.Tests/FolderCatalogTests.cs
@@ -0,0 +1,230 @@
+﻿using Microsoft.Extensions.Logging;
+using Modaularity.Abstractions;
+using Modaularity.Catalogs.Assemblies;
+using Modaularity.Catalogs.Folders;
+using Modaularity.Context;
+using Modaularity.TypeFinding;
+using Newtonsoft.Json;
+using System.Reflection;
+
+namespace Modaularity.Tests;
+
+public class FolderCatalogTests
+{
+    private readonly string _moduleFolder =
+        $"..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}Assemblies{Path.DirectorySeparatorChar}output{Path.DirectorySeparatorChar}net7.0";
+
+    [Fact]
+    public async Task CanInitialize()
+    {
+        var catalog = new FolderModuleCatalog(_moduleFolder);
+
+        await catalog.Initialize();
+
+        var modules = catalog.GetModules();
+
+        Assert.NotEmpty(modules);
+    }
+
+    [Fact]
+    public async Task CanInitializeWithCriteria()
+    {
+        var catalog = new FolderModuleCatalog(_moduleFolder, configure =>
+        {
+            configure.HasName("*Module*");
+        });
+
+        await catalog.Initialize();
+
+        var moduleCount = catalog.GetModules().Count();
+
+        Assert.Equal(2, moduleCount);
+    }
+
+    [Fact]
+    public async Task CanUseFolderOptions()
+    {
+        var options = new FolderModuleCatalogOptions
+        {
+            TypeFinderOptions = new()
+            {
+                TypeFinderCriterias = new()
+                {
+                    TypeFinderCriteriaBuilder.Create().HasName("SecondModule").Tag("MyModule")
+                }
+            }
+        };
+
+        var catalog = new FolderModuleCatalog(_moduleFolder, options);
+
+        await catalog.Initialize();
+
+        var moduleCount = catalog.GetModules().Count;
+
+        Assert.Equal(1, moduleCount);
+        Assert.Equal("SecondModule", catalog.Single().Type.Name);
+    }
+
+    [Fact]
+    public async Task FolderOptionsAreUsedToLimitLoadedAssemblies()
+    {
+        var options = new FolderModuleCatalogOptions 
+        { 
+            TypeFinderOptions = new()
+            {
+                TypeFinderCriterias = new()
+                {
+                    TypeFinderCriteriaBuilder.Create().HasName("SecondModule").Tag("MyModule")
+                }
+            }
+        };
+
+        var catalog = new FolderModuleCatalog(_moduleFolder, options);
+
+        await catalog.Initialize();
+
+        var field = catalog.GetType().GetField("_catalogs", BindingFlags.Instance | BindingFlags.NonPublic);
+        var loadedAssemblies = (List<AssemblyModuleCatalog>)field.GetValue(catalog);
+
+        Assert.Single(loadedAssemblies);
+    }
+
+    [Fact]
+    public async Task CanConfigureNamingOptions()
+    {
+        var options = new FolderModuleCatalogOptions()
+        {
+            ModuleNameOptions = new()
+            {
+                ModuleNameGenerator = (nameOptions, type) => type.FullName + "Modified"
+            }
+        };
+
+        var catalog = new FolderModuleCatalog(_moduleFolder, options);
+
+        await catalog.Initialize();
+
+        var modules = catalog.GetModules();
+
+        foreach (var module in modules)
+            Assert.EndsWith("Modified", module.Name);
+    }
+
+    [Fact]
+    public async Task CanUseReferencedDependencies()
+    {
+        ModuleLoadContextOptions.Defaults.UseHostApplicationAssemblies = UseHostApplicationAssembliesEnum.Never;
+
+        Action<TypeFinderCriteriaBuilder> configureFinder = configure =>
+        {
+            configure.HasName("*JsonResolver*");
+        };
+
+        var folder1Catalog = 
+            new FolderModuleCatalog($"..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}Assemblies{Path.DirectorySeparatorChar}output{Path.DirectorySeparatorChar}JsonNew{Path.DirectorySeparatorChar}net7.0", configureFinder);
+        var folder2Catalog =
+            new FolderModuleCatalog($"..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}Assemblies{Path.DirectorySeparatorChar}output{Path.DirectorySeparatorChar}JsonOld{Path.DirectorySeparatorChar}net7.0", configureFinder);
+
+        await folder1Catalog.Initialize();
+        await folder2Catalog.Initialize();
+
+        var newModule = folder1Catalog.Single();
+        var oldModule = folder2Catalog.Single();
+
+        dynamic newModuleJsonResolver = Activator.CreateInstance(newModule);
+        var newModuleVersion = newModuleJsonResolver.GetVersion();
+
+        dynamic oldModuleJsonResolver = Activator.CreateInstance(oldModule);
+        var oldModuleVersion = oldModuleJsonResolver.GetVersion();
+
+        Assert.Equal("13.0.0.0", newModuleVersion);
+        Assert.Equal("13.0.0.0", oldModuleVersion);
+    }
+
+    [Fact]
+    public async Task CanUseSelectedHostsDependencies()
+    {
+        var json = JsonConvert.SerializeObject(1);
+        var loggin = new LoggerFactory();
+        var options = new FolderModuleCatalogOptions
+        {
+            TypeFinderCriteria = new()
+            {
+                Name = "*JsonResolver*"
+            },
+            ModuleLoadContextOptions = new()
+            {
+                UseHostApplicationAssemblies = UseHostApplicationAssembliesEnum.Selected,
+                HostApplicationAssemblies = new()
+                {
+                    typeof(LoggerFactory).Assembly.GetName()
+                }
+            }
+        };
+
+        var catalog =
+            new FolderModuleCatalog($"..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}Assemblies{Path.DirectorySeparatorChar}output{Path.DirectorySeparatorChar}JsonOld{Path.DirectorySeparatorChar}net7.0", options);
+
+        await catalog.Initialize();
+
+        var oldModule = catalog.Single();
+        dynamic oldModuleJsonResolver = Activator.CreateInstance(oldModule);
+        var oldModuleVersion = oldModuleJsonResolver.GetVersion();
+        var loggerVersion = oldModuleJsonResolver.GetLoggingVersion();
+
+        Assert.Equal("7.0.0.0", loggerVersion);
+        Assert.Equal("13.0.0.0", oldModuleVersion);
+    }
+
+    [Collection(nameof(NotThreadSafeResourceCollection))]
+    public class DefaultOptions : IDisposable
+    {
+        private readonly string _moduleFolder =
+            $"..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}Assemblies{Path.DirectorySeparatorChar}output{Path.DirectorySeparatorChar}net7.0";
+
+        public DefaultOptions()
+        {
+            FolderModuleCatalogOptions.Defaults.ModuleNameOptions = new()
+            {
+                ModuleNameGenerator = (nameOptions, type) => type.FullName + "Modified"
+            };
+        }
+
+        [Fact]
+        public async Task CanConfigureDefaultNamingOptions()
+        {
+            var catalog = new FolderModuleCatalog(_moduleFolder);
+
+            await catalog.Initialize();
+
+            var modules = catalog.GetModules();
+
+            foreach (var module in modules)
+                Assert.EndsWith("Modified", module.Name);
+        }
+
+        [Fact]
+        public async Task DefaultAssemblyNamingOptionsDoesntAffectFolderCatalogs()
+        {
+            AssemblyModuleCatalogOptions.Defaults.ModuleNameOptions = new()
+            {
+                ModuleNameGenerator = (nameOptions, type) => type.FullName + "ModifiedAssembly"
+            };
+
+            var catalog = new FolderModuleCatalog(_moduleFolder);
+
+            await catalog.Initialize();
+
+            var modules = catalog.GetModules();
+
+            foreach (var module in modules)
+                Assert.False(module.Name.EndsWith("ModifiedAssembly"));
+        }
+
+        public void Dispose()
+        {
+            AssemblyModuleCatalogOptions.Defaults.ModuleNameOptions = new ModuleNameOptions();
+            FolderModuleCatalogOptions.Defaults.ModuleNameOptions = new ModuleNameOptions();
+        }
+    }
+}
