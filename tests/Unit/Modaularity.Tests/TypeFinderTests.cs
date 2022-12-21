497240a84ee075de5ee60c57659d7834d0668d8f
vdaular <vdaular@derkodex.com>
2022-12-21 11:48:24 -0300

    Pruebas de buscador de tipos

---
+++ tests/Unit/Modaularity.Tests/TypeFinderTests.cs
@@ -0,0 +1,33 @@
+﻿using Modaularity.Catalogs.Assemblies;
+using Modaularity.Tests.Modules;
+
+namespace Modaularity.Tests;
+
+public class TypeFinderTests
+{
+    [Fact]
+    public async Task CanGetModulesByAttribute()
+    {
+        var catalog = new AssemblyModuleCatalog(typeof(TypeFinderTests).Assembly, configure => 
+        {
+            configure.HasAttribute(typeof(MyModuleAttribute));
+        });
+
+        await catalog.Initialize();
+
+        Assert.Equal(2, catalog.GetModules().Count);
+    }
+
+    [Fact]
+    public async Task CanGetModulesByMultipleCriteria()
+    {
+        var catalog = new AssemblyModuleCatalog(typeof(TypeFinderTests).Assembly, configure => 
+        {
+            configure.HasAttribute(typeof(MyModuleAttribute)).IsAbstract(true).HasName(nameof(AbstractModuleWithAttribute));
+        });
+
+        await catalog.Initialize();
+
+        Assert.Single(catalog.GetModules());
+    }
+}
