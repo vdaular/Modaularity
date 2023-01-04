ec0848d7ecc642504e626000bcc385a97a901120
Víctor Daniel Aular <vdaular@users.noreply.github.com>
2023-01-04 08:41:08 -0300

    Se agrega proyecto de ejemplo en Blazor Server

---
+++ samples/Modaularity.Samples/Modaularity.Samples.BlazorServerApp/Program.cs
@@ -0,0 +1,33 @@
+using Modaularity.AspNetCore;
+using Modaularity.Samples.BlazorServerApp.Data;
+using Modaularity.Samples.Shared;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddModaularity<IWidget>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
